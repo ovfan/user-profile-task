bd5c19f87567b8c43e703d2803c0168fd81f0cae
fanfan <1763058483@qq.com>
2022-08-10 11:43:42 +0800

    add

--- task-sql/src/main/java/com/atguigu/userprofile/app/TaskSql.java
+++ task-sql/src/main/java/com/atguigu/userprofile/app/TaskSql.java
@@ -10,7 +10,15 @@ package com.atguigu.userprofile.app;
 public class TaskSql {
     public static void main(String[] args) {
         // 1. 获取传递进来的taskId、busiDate(业务日期)
+        // 约定好 (代码中已经写好了)，第一个参数是taskId，第二个参数是业务日期
 
-        // 2.
+
+        // 2.通过tasId从Mysql获取标签信息(标签编码、标签名等) 任务信息(sql) 标签规则信息(查询值和标签对应关系)
+
+        // 3. 建表（如果不存在）
+
+        //4. 拼接 insert select sql
+
+        // 5. 执行SQL
     }
 }
