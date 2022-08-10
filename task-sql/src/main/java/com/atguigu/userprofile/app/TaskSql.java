5305d6117904fbc1148850702d9adb0a44ec23b5
fanfan <1763058483@qq.com>
2022-08-10 11:37:34 +0800

    init

---
+++ task-sql/src/main/java/com/atguigu/userprofile/app/TaskSql.java
@@ -0,0 +1,16 @@
+package com.atguigu.userprofile.app;
+
+/**
+ * @ClassName: TaskSql
+ * @Description: 处理SQL任务
+ * @Author: fanfan
+ * @DateTime: 2022年08月10日 11时34分
+ * @Version: v1.0
+ */
+public class TaskSql {
+    public static void main(String[] args) {
+        // 1. 获取传递进来的taskId、busiDate(业务日期)
+
+        // 2.
+    }
+}
